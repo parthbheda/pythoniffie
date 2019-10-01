3b44359cc50ffc163d586dc4f932ccd83bbf298f
sekhar989 <krishnendu.s.kar@gmail.com>
2019-10-01 20:01:44 +0100

    num_test

---
+++ num_pos_neg_zero.py
@@ -0,0 +1,12 @@
+num = 3.4
+
+# Try these two variations as well:
+# num = 0
+# num = -4.5
+
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
