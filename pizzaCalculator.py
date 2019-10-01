253ad0441246b2103ed8e7e04dd5b6ac536ba1d1
Dennis <dennis.a.schaffer@icloud.com>
2019-10-01 19:25:28 +0200

    add python script: pizza calculator

---
+++ pizzaCalculator.py
@@ -0,0 +1,12 @@
+import math
+people = int(input('How much people? '))
+
+pizza = 1.5
+
+ergebnis = int(round(people * pizza))
+
+if people >= 10:
+    ergebnis = ergebnis +1
+    print(math.ceil(ergebnis))
+else:
+    print(math.ceil(ergebnis))
