8441ffd8caa6efa170b0cb3f943917948f72fcd0
senthilraja112 <sssenthilselva1997@gmail.com>
2019-10-03 23:11:42 +0530

    Update minimum_of_3.py

--- minimum_of_3.py
+++ minimum_of_3.py
@@ -1,9 +1,10 @@
-a = input("Enter first num")
-b = input("Enter second num")
-c = input("Enter third num")
-if a > c and b > c:
-    print(str(c)+" is smallest.")
-elif a > b and c > b:
-    print(str(b)+" is smallest.")
+num1 = int(input('Enter First number : '))
+num2 = int(input('Enter Second number : '))
+num3 = int(input('Enter Third number : '))
+if (num1 < num2) and (num1 < num3):
+        smallest_num = num1
+elif (num2 < num1) and (num2 < num3):
+        smallest_num = num2
 else:
-    print(str(a)+" is smallest.")
+        smallest_num = num3
+    print("The smallest of the 3 numbers is : ", smallest_num)
