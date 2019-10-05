73d4c0e8b5ff9ad42f4208458222450e7f4d8446
NerdOfCode <admin@nerdofcode.com>
2019-10-05 09:26:07 -0500

    Updated formatting...
    Updated formatting, and added useful comment explaining Python's datetime module

--- check_monday.py
+++ check_monday.py
@@ -1,10 +1,14 @@
 import datetime
 
-
-
 print('Is Today Monday?')
 
 today = datetime.date.today()
+
+# today.weekday() output:
+# Sunday  = 0
+# Monday  = 1
+# Tuesday = 2
+# ...
 
 if today.weekday() == 1:
     print('Yes.')
