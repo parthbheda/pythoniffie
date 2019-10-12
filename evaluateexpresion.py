f6ff9628eda1d9cf0f7d28f8662c2be2a645da94
ChrisBlesson2598 <blesson2598@gmail.com>
2019-10-12 19:23:25 +0530

    expression evaluation

---
+++ evaluateexpresion.py
@@ -0,0 +1,9 @@
+#This program receives an expression say 1+2*3+45*76
+#Evalutes it and prints the result
+#get the expression
+expr=input() 
+#use eval() method an inbuilt python method that takes a string
+# and returns the output
+print(eval(expr))
+# 1+2*3+45*76
+# op:3427
