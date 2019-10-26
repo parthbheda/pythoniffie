a465fce9722d2ab0253653f98c89c56cc110efcf
gtanishka8 <gtanishka8@gmail.com>
2019-10-26 14:08:46 +0530

    changes done

--- bartender.py
+++ bartender.py
@@ -1,15 +1,10 @@
 # A little python program in light of Hacktoberfest 2019 :)
 
-legal_age = 21
-
-print("-- Welcome to the Hacktoberfest bar --")
-
+legalage = 21
 age = int(input("How old are you? "))
 
-if age >= legal_age:
+if age >= legalage:
     print("Have a beer!")
-elif age == 20:
-    print("So close..one more year kid.")
 else:
-    print(f"Sorry, {age} is too young for a beer, how about a soda?")
+    print("Sorry",age," is too young for a beer, how about a soda?")
     
