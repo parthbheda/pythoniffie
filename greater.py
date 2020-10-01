e2f246258467d71374e4b75925fdb3f8ea8aaa21
zaiddx <72139621+zaiddx@users.noreply.github.com>
2020-10-01 11:55:31 +0530

    Update greater.py
    making program strong to counter wrong input

--- greater.py
+++ greater.py
@@ -1,8 +1,10 @@
 a = int(input())
 b = int(input())
-if b > a:
+if b>a:
   print("b is greater than a")
 elif a>b:
   print("a is greater than b")
-else:
+elif a==b:
   print("Both are equal")
+else:
+  print("Wrong input")
