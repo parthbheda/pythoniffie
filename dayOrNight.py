7ae28d4d508e2d02f8bb144e398b7d7bdfd9bfad
Dipanshu <dipanshu231099@gmail.com>
2020-10-03 15:24:44 +0530

    added something more

--- dayOrNight.py
+++ dayOrNight.py
@@ -1,8 +1,9 @@
 from datetime import datetime as dt
 
 hour = int(dt.now().strftime("%H"))
+time = dt.now().strftime("%H:%M:%S")
 
 if(hour >= 19):
-    print("Night")
+    print("time is",time,".Thus it is","Night")
 else:
-    print("Day")
+    print("time is",time,".Thus it is","Day")
