98139f36ff322d4060f68be02a8473a2661a990c
ninja <dipanshu2399@gmail.com>
2020-10-03 15:30:49 +0530

    added decision according to user

--- dayOrNight.py
+++ dayOrNight.py
@@ -3,7 +3,12 @@ from datetime import datetime as dt
 hour = int(dt.now().strftime("%H"))
 time = dt.now().strftime("%H:%M:%S")
 
-if(hour >= 19):
+yourDivider = int(input("After what hour(24 Hour system) you prefer Night: "))
+while not(1<=yourDivider<=23):
+    print("Please enter a valid Time when your night starts")
+    yourDivider = int(input("After what hour(24 Hour system) you prefer Night: "))
+
+if(hour >= yourDivider):
     print("time is",time,".Thus it is","Night")
 else:
     print("time is",time,".Thus it is","Day")
